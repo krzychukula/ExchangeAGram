2ba6c58b0f712c41015dd196e54941ec9502fb25
krzychukula <krzychukula@gmail.com>
2014-12-27 17:17:29 +0100

    Custom Cell Class

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,13 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Krzysztof Kula on 27/12/14.
+//  Copyright (c) 2014 Krzysztof Kula. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+}
