e286440927bb080f157e89aab4cc36fda180853c
krzychukula <krzychukula@gmail.com>
2014-12-28 11:20:58 +0100

    use photo library or show alert

--- ExchangeAGram/FeedViewController.swift
+++ ExchangeAGram/FeedViewController.swift
@@ -37,6 +37,20 @@ class FeedViewController: UIViewController, UICollectionViewDataSource, UICollec
             cameraController.allowsEditing = false
             
             self.presentViewController(cameraController, animated: true, completion: nil)
+        }else if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            var photoLibraryController = UIImagePickerController()
+            photoLibraryController.delegate = self
+            photoLibraryController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            
+            let mediaTypes:[AnyObject] = [kUTTypeImage]
+            photoLibraryController.mediaTypes = mediaTypes
+            photoLibraryController.allowsEditing = false
+            
+            self.presentViewController(photoLibraryController, animated: true, completion: nil)
+        }else{
+            var alertView = UIAlertController(title: "Alert", message: "Your device does not support the camera or photo Library", preferredStyle: UIAlertControllerStyle.Alert)
+            alertView.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alertView, animated: true, completion: nil)
         }
     }
     
