d840326473a0e67cc28de4e0d8c940846026a4b6
krzychukula <krzychukula@gmail.com>
2015-01-04 21:47:10 +0100

    Add bridging header

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Krzysztof Kula on 04/01/15.
+//  Copyright (c) 2015 Krzysztof Kula. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
