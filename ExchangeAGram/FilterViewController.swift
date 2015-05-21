36d3a5f1868e3a6e1bdc5f145346a1387b91e6a5
krzychukula <krzychukula@gmail.com>
2015-05-21 08:29:49 +0200

    Facebook share and save caption

--- ExchangeAGram/FilterViewController.swift
+++ ExchangeAGram/FilterViewController.swift
@@ -94,23 +94,22 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
             textField.secureTextEntry = false//for secure true
         }
         
-        var text:String
         let textField = alert.textFields![0] as! UITextField
-        
-        if (textField.text != nil) {
-            text = textField.text
-        }
         
         let photoAction = UIAlertAction(title: "Post Photo to Facebook with Caption", style: UIAlertActionStyle.Destructive,
         handler: { (UIAlertAction) -> Void in
             
-            self.saveFilterToCoreData(indexPath)
+            self.shareToFacebook(indexPath)
+            
+            var text = textField.text
+            self.saveFilterToCoreData(indexPath, caption: text)
         })
         alert.addAction(photoAction)
         
         let saveFilterAction = UIAlertAction(title: "Save Filter without posting on Facebook", style: UIAlertActionStyle.Default) { (UIAlertAction) -> Void in
             
-            self.saveFilterToCoreData(indexPath)
+            var text = textField.text
+            self.saveFilterToCoreData(indexPath, caption: text)
         }
         alert.addAction(saveFilterAction)
         
@@ -171,7 +170,7 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         return finalImage!
     }
     
-    func saveFilterToCoreData(indexPath: NSIndexPath) {
+    func saveFilterToCoreData(indexPath: NSIndexPath, caption: String) {
         let filteredImage = self.filteredImageFromImage(self.thisFeedItem.image, filter: self.filters[indexPath.row])
         
         let imageData = UIImageJPEGRepresentation(filteredImage, 1.0)
@@ -180,9 +179,26 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         let thumbData = UIImageJPEGRepresentation(filteredImage, 0.1)
         self.thisFeedItem.thumbNail = thumbData
         
+        self.thisFeedItem.caption = caption
+        
         (UIApplication.sharedApplication().delegate as! AppDelegate).saveContext()
         
         self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    func shareToFacebook(indexPath: NSIndexPath){
+        let filteredImage = self.filteredImageFromImage(self.thisFeedItem.image, filter: self.filters[indexPath.row])
+        let photos:NSArray = [filteredImage] as NSArray
+        var params = FBPhotoParams()
+        params.photos = photos as [AnyObject]
+        FBDialogs.presentMessageDialogWithPhotoParams(params, clientState: nil) {
+            (call, result, error) -> Void in
+            if(result != nil){
+                println(result)
+            }else{
+                println(error)
+            }
+        }
     }
     
     //caching functions
