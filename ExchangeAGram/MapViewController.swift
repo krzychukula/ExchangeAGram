e2ffa42a85ad8eeef93b8d623557a0308680180a
krzychukula <krzychukula@gmail.com>
2015-05-24 20:27:26 +0200

    add MapKit

---
+++ ExchangeAGram/MapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MapViewController.swift
+//  ExchangeAGram
+//
+//  Created by Krzysztof Kula on 24/05/15.
+//  Copyright (c) 2015 Krzysztof Kula. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
