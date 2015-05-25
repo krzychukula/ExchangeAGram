52bc820f72b7d939cf493c08013bd9ade568cd05
krzychukula <krzychukula@gmail.com>
2015-05-25 11:31:29 +0200

    update model

--- ExchangeAGram/FeedItem.swift
+++ ExchangeAGram/FeedItem.swift
@@ -2,7 +2,7 @@
 //  FeedItem.swift
 //  ExchangeAGram
 //
-//  Created by Krzysztof Kula on 01/01/15.
+//  Created by Krzysztof Kula on 25/05/15.
 //  Copyright (c) 2015 Krzysztof Kula. All rights reserved.
 //
 
@@ -10,10 +10,13 @@ import Foundation
 import CoreData
 
 @objc(FeedItem)
+
 class FeedItem: NSManagedObject {
 
     @NSManaged var caption: String
     @NSManaged var image: NSData
     @NSManaged var thumbNail: NSData
+    @NSManaged var latitude: NSNumber
+    @NSManaged var longitude: NSNumber
 
 }
