f7202a19af9587ad34ec146ca9432c73105a4d44
krzychukula <krzychukula@gmail.com>
2015-05-26 07:53:42 +0200

    request for feed items

--- ExchangeAGram/MapViewController.swift
+++ ExchangeAGram/MapViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class MapViewController: UIViewController {
 
@@ -18,19 +19,26 @@ class MapViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         
-        let location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        let region = MKCoordinateRegionMake(location, span)
+        let request = NSFetchRequest(entityName: "FeedItem")
+        let appDelegate:AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        let context:NSManagedObjectContext = appDelegate.managedObjectContext!
+        let error: NSError?
+        let itemArray = context.executeFetchRequest(request, error: &error);
+        print(error)
         
-        mapView.setRegion(region, animated: true)
-        
-        
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        annotation.title = "Canal Saint-Martin"
-        annotation.subtitle = "Paris"
-        
-        mapView.addAnnotation(annotation)
+//        let location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
+//        let span = MKCoordinateSpanMake(0.05, 0.05)
+//        let region = MKCoordinateRegionMake(location, span)
+//        
+//        mapView.setRegion(region, animated: true)
+//        
+//        
+//        let annotation = MKPointAnnotation()
+//        annotation.coordinate = location
+//        annotation.title = "Canal Saint-Martin"
+//        annotation.subtitle = "Paris"
+//        
+//        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
