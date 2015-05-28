44ea4f7d99de34b53a185c95faac68f538698e43
krzychukula <krzychukula@gmail.com>
2015-05-28 08:12:31 +0200

    show photos on the map

--- ExchangeAGram/MapViewController.swift
+++ ExchangeAGram/MapViewController.swift
@@ -22,9 +22,25 @@ class MapViewController: UIViewController {
         let request = NSFetchRequest(entityName: "FeedItem")
         let appDelegate:AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
         let context:NSManagedObjectContext = appDelegate.managedObjectContext!
-        let error: NSError?
+        var error: NSError?
         let itemArray = context.executeFetchRequest(request, error: &error);
         print(error)
+        
+        if itemArray!.count > 0 {
+            for item in itemArray! {
+                let location = CLLocationCoordinate2D(latitude: Double(item.latitude), longitude: Double(item.longitude))
+                let span = MKCoordinateSpanMake(0.05, 0.05)
+                let region = MKCoordinateRegionMake(location, span)
+                mapView.setRegion(region, animated: true)
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = location
+                annotation.title = item.caption
+                
+                mapView.addAnnotation(annotation)
+            }
+            
+        }
         
 //        let location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
 //        let span = MKCoordinateSpanMake(0.05, 0.05)
